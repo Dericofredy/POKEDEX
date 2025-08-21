681f5a8489b22b055fd0f342620bf42a8449fe49
Frederico Caldeira <60188689+Dericofredy@users.noreply.github.com>
2025-08-21 13:00:29 -0300

    atualização css e js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+// Referências aos elementos HTML
 const imgFechada = document.getElementById('pokedex-fechada');
 const imgAberta = document.getElementById('pokedex-aberta');
 const tela = document.getElementById('tela-pokedex');
 const btnIniciar = document.getElementById('btn-iniciar');
 
-// Ao clicar na Pokédex fechada → abrir
+// Ao clicar na imagem da Pokédex fechada, mostrar aberta
 imgFechada.addEventListener('click', () => {
-  imgFechada.classList.add('hidden');
-  imgAberta.classList.remove('hidden');
-  tela.classList.remove('hidden');
+  imgFechada.classList.add('hidden');      // Esconde a pokédex fechada
+  imgAberta.classList.remove('hidden');    // Mostra a pokédex aberta
+  tela.classList.remove('hidden');         // Mostra a tela com botão
 });
 
-// Ao clicar no botão iniciar → abrir pokedex.html
+// Ao clicar no botão "Iniciar Pokédex", vai para pokedex.html
 btnIniciar.addEventListener('click', () => {
   window.location.href = 'pokedex.html';
 });
